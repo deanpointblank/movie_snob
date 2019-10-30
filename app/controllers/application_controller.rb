67786ec426e5bafba9fe7992aac1adea527d87ee
dean <deanlloyd64@gmail.com>
2019-10-29 23:22:46 -0400

    enable login for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,23 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    #set :session_secret
   end
 
   get "/" do
     erb :welcome
   end
 
+  get helpers do
+
+    def logged_in?
+      session[:user_id]
+    end
+
+    def current_user
+      @user ||= User.find_by(id: session[:user_id])
+    end
+
+  end
 end
