0e24703a375a93390a07733b30ac90cf2b2410ea
dean <deanlloyd64@gmail.com>
2019-11-01 18:04:52 -0400

    strip movie_controller features

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,17 +2,9 @@ class MoviesController < ApplicationController
 
   # GET: /movies
   get "/movies" do
+    binding.pry
+    @movie = Movie.find_by_id(params["id"])
     erb :"/movies/index.html"
-  end
-
-  # GET: /movies/new
-  get "/movies/new" do
-    erb :"/movies/new.html"
-  end
-
-  # POST: /movies
-  post "/movies" do
-    redirect "/movies"
   end
 
   # GET: /movies/5
@@ -21,18 +13,4 @@ class MoviesController < ApplicationController
     erb :"/movies/show.html"
   end
 
-  # # GET: /movies/5/edit
-  # get "/movies/:id/edit" do
-  #   erb :"/movies/edit.html"
-  # end
-
-  # # PATCH: /movies/5
-  # patch "/movies/:id" do
-  #   redirect "/movies/:id"
-  # end
-
-  # # DELETE: /movies/5/delete
-  # delete "/movies/:id/delete" do
-  #   redirect "/movies"
-  # end
 end
